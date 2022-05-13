2ba076a3145e09d591f9672dff86289a79dc5953
RCvetinskis <robertas.cvetinskis@gmail.com>
2022-05-13 11:30:19 +0300

    complete calculator

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ var deleteButton = document.querySelector("[data-delete]")
 var allClearButton = document.querySelector("[data-all-clear]")
 var previousTextElement = document.querySelector("[data-previous-opperand]")
 var currentTextElement = document.querySelector("[data-currenct-opperand]")
+var result = false
 
 
 class Calculator {
@@ -18,14 +19,30 @@ class Calculator {
     }
   //Paruosia darbui objekta skaiciu ir simboliu parametrus
   clear(){
-      this.currentOpperand = ""
+      this.currentOpperand = "" 
       this.previousOpperand = ""
       this.operation = undefined
+  }
+  //istrina po viena
+  delete(){
+ 
+      this.currentOpperand = this.currentOpperand.slice(0, -1)
   }
 
   appendNumber(number){
       // salyga neleis daugiau nei vieno tasko prideti
       if(number === "." && this.currentOpperand.includes(".")) return
+      if(result && number && number !== "." && number != 0 ){
+          this.currentOpperand = ""
+          result= false
+      } else if (result && number == ".")  {
+              this.currentOpperand= "0"
+      }else if (result && number == "0" && !this.currentOpperand.includes("0")){
+          this.currentOpperand =""
+          result = false
+      } else if(this.currentOpperand.includes("0") && !this.currentOpperand.includes("0")){
+          this.currentOpperand = ""
+      }
       this.currentOpperand += number
   }
 
@@ -34,15 +51,43 @@ class Calculator {
       if(this.operation !== undefined){
         this.previousTextElement.innerText = 
         `${this.previousOpperand} ${this.operation}`
+      } else {
+          this.previousTextElement.innerText = ""
       }
 
   }
 
   chooseOperation(operation){
       if(this.currentOpperand == "") return
+      if(this.previousOpperand!== ""){
+          this.compute()
+      }
       this.operation = operation
       this.previousOpperand = this.currentOpperand
       this.currentOpperand = ""
+  }
+  //skaiciavimas
+  compute(){
+      var computation
+      var current = parseFloat(this.currentOpperand)
+      var prev = parseFloat(this.previousOpperand)
+       if(isNaN(current) || isNaN(prev)) return
+
+      switch(this.operation){
+          case "+": computation = prev + current;
+          break;
+          case "-": computation = prev - current;
+          break;
+          case "*": computation = prev * current;
+          break;
+          case "÷": computation = prev / current;
+          break;
+          default:return;
+      }
+      this.currentOpperand = computation.toString()
+      this.previousOpperand = ""
+      this.operation = undefined
+     
   }
 }
 //sukura skaiciatuvo objekta atmentyje
@@ -63,6 +108,23 @@ operationsButtons.forEach(x => {
     x.addEventListener("click", function(){
        calculator.chooseOperation(this.innerText)
        calculator.updateDisplay()
-     
     })
 })
+
+equalsButton.addEventListener("click", function(){
+    calculator.compute()
+    calculator.updateDisplay()
+    result= true
+ })
+
+
+ allClearButton.addEventListener("click", function(){
+    calculator.clear()
+    calculator.updateDisplay()
+ })
+ deleteButton.addEventListener("click", function(){
+    calculator.delete()
+    calculator.updateDisplay()
+ })
+
+ 
